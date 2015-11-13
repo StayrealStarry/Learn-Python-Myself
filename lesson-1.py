ddec1d11556b52c15ebe0cf33192f65add6c1f73
StayrealS <maidouxinyi1994@qq.com>
2015-11-13 22:47:50 +0800

    lesson 1

---
+++ lesson-1.py
@@ -0,0 +1,25 @@
+# print "Hello,World!"
+
+# name = raw_input("What is your name?")
+# print "Hello. "+ name +"!"
+
+# temp = 22
+# print "Hello! " + repr(temp)
+
+'''
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+ending = ['st','nd','rd']+ 17*['th'] +['st','nd','rd']+ 7*['th'] +['st']
+
+year = raw_input ('Year: ')
+month = raw_input ('Month: ')
+day = raw_input ('Day: ')
+
+month_num = int(month)
+day_num = int(day)
+
+month_name = months[month_num - 1]
+day_name = repr(day_num)+ending[day_num - 1]
+
+print month_name + " " + day_name + " " + year
+'''
+
